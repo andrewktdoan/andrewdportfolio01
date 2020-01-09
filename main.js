1dc487e4f02a9ef2bb0a7ba4940177288d8e4563
Andrew Doan <andrew@andrewdoan.net>
2020-01-08 22:23:10 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Hamburger Menu toggle
+const toggleButton = document.querySelector(".toggle-button");
+const navbarLinks = document.querySelector(".navbar-links");
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
